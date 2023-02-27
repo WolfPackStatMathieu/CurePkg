b0cdc16d374ed2e1992137ad220057704c81e317
Nathan GORSE <gorsenath@gmail.com>
2023-02-27 17:40:58 +0100

    modif weibull et print_diff

--- R/print_diffs.R
+++ R/print_diffs.R
@@ -20,30 +20,32 @@
 #' lmoins<-1
 #' l_plus<-1000
 #' N<-50
-#' test_plot<-fonction_compar_plots(limit_sup = l_plus,limit_inf = lmoins,N=N,p=p2,lambda=lambda7,t_star=t_star,K=k)
+#' shape<-2
+#' test_plot<-fonction_compar_plots(limit_sup = l_plus,limit_inf = lmoins,N=N,p=p2,lambda=lambda7,t_star=t_star,K=k,sh=shape)
 
-fonction_compar_plots<-function(limit_inf,limit_sup,N,p,lambda,t_star,K){
+fonction_compar_plots<-function(limit_inf,limit_sup,N,p,lambda,t_star,K,sh){
+  require(gridExtra)
   vector_size<-sample(c(limit_inf:limit_sup),N)
   vector_size<-vector_size[order(vector_size)]
   liste_param1<-list(p)
   names(liste_param1)<-c("p")
   modele_bern<-"bernoulli"
   result1<-fonction_generation_taille_mean(vector_size,modele_bern,liste_param1,K)
-  liste_param2<-list(lambda,t_star)
-  names(liste_param2)<-c("lambda","t_star")
-  modele_exp<-"surv"
+  liste_param2<-list(lambda,t_star,sh)
+  names(liste_param2)<-c("lambda","t_star","k")
+  modele_exp<-"weibull"
   result2<-fonction_generation_taille_mean(vector_size,modele_exp,liste_param2,K)
   whole_data_expbern<-cbind.data.frame(vector_size,result1,result2)
   colnames(whole_data_expbern)<-c("Size","Mean_Bias_Bern","Mean_Bias_Surv")
-  ####plot
+  ####plot 
   gg1<-ggplot(data=whole_data_expbern,aes(x=Size,y=Mean_Bias_Bern))+
     geom_point(colour="red")+
     labs(y="Mean Bias with Bern model")
-
+  
   gg2<-ggplot(data=whole_data_expbern,aes(x=Size,y=Mean_Bias_Surv))+
     geom_point(colour="blue")+
     labs(y="Mean Bias with Surv model")
-
+  
   whole_g<-gridExtra::grid.arrange(gg1,gg2,ncol=2,top="Comparison of the two methods")
   return(whole_g)
 }
