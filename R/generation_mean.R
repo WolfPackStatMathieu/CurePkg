017a3e1c13b6358c8d7b11b3bd4746722f991647
Nathan GORSE <gorsenath@gmail.com>
2023-03-07 22:04:03 +0100

    modification generation_mean

--- R/generation_mean.R
+++ R/generation_mean.R
@@ -13,51 +13,32 @@
 #' @examples
 #' #test avec le mod√®le "survie"
 #' #---------------------------
-#' N<-100
-#' vecteur_size<-sample(c(1:1000),N)
-#' lambda_test<-0.33
+#' N<-10
+#' vecteur_size<-sample(c(1:100),N)
+#' lamdba_test<-3
 #' t_star<-6
-#' liste_parameter<-list(lambda_test,t_star)
-#' names(liste_parameter)<-c("lambda","t_star")
-#' modele<-"surv"
-#' k<-20
-#' res <- fonction_generation_taille_mean(vector_size=vecteur_size,model=modele,K=k,liste_parameter = liste_parameter)
-#'
-#' #plot des r√©sultats
-#' #------------------
-#' donnees_taille_biaismoyen<-cbind.data.frame(vecteur_size[order(vecteur_size)],res)
-#' colnames(donnees_taille_biaismoyen)<-c("Size","Mean_Bias")
-#' plot(donnees_taille_biaismoyen,main="The mean bias according to the size with Survival function")
-#'
-#' #test avec le mod√®le bernouilli
-#' #------------------------------
-#' prop<-0.33
-#' liste_param<-list(prop)
-#' names(liste_param)<-c("p")
-#' modele1<-"bernoulli"
-#' test_bern_taillemoy<-fonction_generation_taille_mean(vector_size=vecteur_size,model=modele1,K=k,liste_parameter = liste_param)
-#'
-#' #plot des resultats
-#' #------------------
-#' donnees_bern_biaismoyen<-cbind.data.frame(vecteur_size[order(vecteur_size)],test_bern_taillemoy)
-#' plot(donnees_bern_biaismoyen,main="The mean bias according to the size with Bernoulli")
-#'
-fonction_generation_taille_mean<-function(vector_size,model,liste_parameter,K){
+#' p<-0.33
+#' k<-1
+#' liste_parameter<-list(lambda_test,t_star,p=p,k)
+#' names(liste_parameter)<-c("lambda","t_star","p","k")
+#' K2<-20
+#' test_exp_taillemoy<-fonction_generation_taille_mean(vector_size=vecteur_size,K=K2,liste_parameter = liste_parameter)
+#' #################### Plot des rÈsultats en fonction de la taille.########
+#' donnees_taille_biaismoyen<-cbind.data.frame(vecteur_size[order(vecteur_size)],test_exp_taillemoy)
+#' colnames(donnees_taille_biaismoyen)<-c("Size","Mean_Bias_Cure","Mean_Bias_Surv")
+#' plot(donnees_taille_biaismoyen$Size,donnees_taille_biaismoyen$Mean_Bias_Cure,main="The mean bias according to the size with Survival function")
+#' points(x=donnees_taille_biaismoyen$Size,y=donnees_taille_biaismoyen$Mean_Bias_Surv,col="red")
+fonction_generation_taille_mean<-function(vector_size,liste_parameter,K){
+  ### renvoie la gÈnÈration avec des tailles diffÈrentes du modËle model (string) ayant comme paramËtre la liste_parameter, 
+  ### liste de paramËtres avec le modËles. 1 seul comme bernoulli et 2 pour exp() (lambda et t_star).
   vector_size<-vector_size[order(vector_size)]
-  if (model=="bernoulli"){
-    vecteur_realisation<-sapply(vector_size,Simuler_Nfois_n_echantillons_bern,N=K,p=liste_parameter[["p"]])
-    return(colMeans(vecteur_realisation))
-  }
-  else{if(model=="surv"){
-    vecteur_realisation<-sapply(vector_size,Simuler_Nfois_n_echantillons,N=K,lambda=liste_parameter[["lambda"]],t_star=liste_parameter[["t_star"]])
-    return(colMeans(vecteur_realisation))
-  }
-    if(model=="weibull"){
-      vecteur_realisation<-sapply(vector_size,Simuler_Nfois_n_weibull,N=K,lambda=liste_parameter[["lambda"]],t_star=liste_parameter[["t_star"]],
-                                  k=liste_parameter[["k"]])
-      return(colMeans(vecteur_realisation))
-    }}
+  ##### idÈe. 
+  Value_bias<-lapply(vector_size,Simuler_biais_taillen,K=K,lambda=liste_parameter[['lambda']],t_star=liste_parameter[["t_star"]],
+                     p=liste_parameter[["p"]],k=liste_parameter[["k"]])
+  value_means<-as.data.frame(t(sapply(Value_bias,colMeans)))
+  value_means$Modele_survie<-value_means$Modele_survie-p
+  value_means$Modele_guerison<-value_means$Modele_guerison-p
+  return(value_means)
 }
-
 
 
