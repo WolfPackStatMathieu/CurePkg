8696165803a375f2d1ea3213d29d8dceb2c70138
GORSE Nathan <id2021@DOMENSAI.ECOLE>
2023-05-30 14:56:00 +0200

    travail

--- R/scenario1.R
+++ R/scenario1.R
@@ -166,85 +166,7 @@ biais.selon.k <-function(K, n, lambda, t_star,p){
   }
   return(results)
 }
-fnct_compar_plt_biais.selon.k<-function(N, n, window_lambda,t_star,p){
-  #' Plot des valeurs des biais moyens selon la taille des echantillons et du lambda.
-  #'
-  #' @param N  nombre de tailles d'echantillon differents.
-  #' @param window_lambda  ensemble des valeurs de lambda considérées.
-  #' @param t_star fin de la fenetre d'observation
-  #' @param n nombre d'individus considérés.
-  #' @param t_tsar fin de la fenêtre d'observation.
-  #' @param
-  #'
-  #' @return Plot des valeurs des biais moyens en fonction du lambda et de la taille des echantillons.
-  #' @export
-  #'
-  #' @examples
-  #' ######Test ######
-  #' plot<-fnct_compar_plt_biais.selon.k(N=100,n=100,window_lambda=c(1,2,4),t_tsar=6,p=0.33)
 
-
-  set.seed(12345)
-  RES<- NULL
-  RES<- biais.selon.k(N, n, window_lambda[1],t_star,p=p)
-  RES0.2.3<-data.frame(RES)
-
-  colnames( RES0.2.3)<- c("k","mean.surv", "mean.cure", "mean.bernoulli")
-  set.seed(12345)
-  RES<- NULL
-  RES<- biais.selon.k(N, n, window_lambda[2],t_star,p=p)
-  RES0.5.3<-data.frame(RES)
-  colnames( RES0.5.3)<- c("k","mean.surv", "mean.cure", "mean.bernoulli")
-
-  set.seed(12345)
-  RES<- NULL
-  RES<- biais.selon.k(N,n, window_lambda[3],t_star,p=p)
-  RES0.1.3<-data.frame(RES)
-  colnames( RES0.1.3)<- c("k","mean.surv", "mean.cure", "mean.bernoulli")
-
-
-  borne_min<-min(min(RES0.5.3$mean.surv),min(RES0.1.3$mean.surv),min(RES0.2.3$mean.surv))
-  borne_max<-max(max(RES0.5.3$mean.surv),max(RES0.1.3$mean.surv),max(RES0.5.3$mean.surv))
-
-  borne_min.c <- min(min(RES0.5.3$mean.cure),min(RES0.1.3$mean.cure),min(RES0.2.3$mean.cure))
-  borne_max.c <- max(max(RES0.5.3$mean.cure),max(RES0.1.3$mean.cure),max(RES0.5.3$mean.cure))
-
-  borne_min.b <- min(min(RES0.5.3$mean.bernoulli),min(RES0.1.3$mean.bernoulli),min(RES0.2.3$mean.bernoulli))
-  borne_max.b <- max(max(RES0.5.3$mean.bernoulli),max(RES0.1.3$mean.bernoulli),max(RES0.5.3$mean.bernoulli))
-
-  par(mfrow=c(2,2))
-  plot(RES0.2.3$k,RES0.2.3$mean.surv,main="Mod?le de survie",
-       ylim=c(-0.1+borne_min,borne_max+0.1),
-       type='b',xlab=expression(alpha),ylab="biais moyen")
-  lines(RES0.5.3$k,RES0.5.3$mean.surv,type="b",col="blue")
-  lines(RES0.1.3$k,RES0.1.3$mean.surv,type="b",col="red")
-  abline(h=0)
-
-
-  plot(RES0.2.3$k,RES0.2.3$mean.cure, main = "Mod?le de gu?rison",
-       ylim=c(-0.1+borne_min.c,borne_max.c+0.1),
-       type='b',xlab=expression(alpha),ylab="biais moyen")
-  lines(RES0.5.3$k,RES0.5.3$mean.cure,type="b",col="blue")
-  lines(RES0.1.3$k,RES0.1.3$mean.cure,type="b",col="red")
-  abline(h=0)
-
-  plot(RES0.2.3$k,RES0.2.3$mean.bernoulli,main="Mod?le de Bernoulli",
-       ylim=c(-0.1+borne_min.b,borne_max.b+0.1),
-       type='b',xlab=expression(alpha),ylab="biais moyen")
-  lines(RES0.5.3$k,RES0.5.3$mean.bernoulli,type="b",col="blue")
-  lines(RES0.1.3$k,RES0.1.3$mean.bernoulli,type="b",col="red")
-  abline(h=0)
-  legend("topright",
-         c("0.1","0.2","0.5"),
-         col=c("red","black","blue"),
-         lty=1)
-  plot.new()
-  legend("topright",
-         c("0.1","0.2","0.5"),
-         col=c("red","black","blue"),
-         lty=1)
-  mtext("Influence de alpha", side = 3, line = -24, outer = TRUE)
-}
 #' Ggplot des valeurs des biais moyens selon la taille des echantillons et du lambda.
 #'
 #' @param N  nombre de tailles d'echantillon differents.
@@ -388,51 +310,6 @@ biais.selon.lambda <-function(K, lambda, t_star,p, k){
   }
   return(results)
 }
-fonction_compar_plotsn_lambda<-function(N,window_lambda,t_star,p,k){
-  #' Plot des valeurs des biais moyens selon la taille des echantillons et du lambda.
-  #'
-  #' @param N nombre de tailles d'echantillon differents.
-  #' @param window_lambda
-  #' @param t_star fin de la fenetre d'observation
-  #'
-  #' @return Plot des valeurs des biais moyens en fonction du lambda et de la taille des echantillons.
-  #' @export
-  #'
-  #' @examples
-  #' ######Test ######
-  #' t_star<-3
-  #' N<-10
-  #' window_lambda<-c(0.2,0.5,0.1)
-  #' result<-fonction_compar_plotsn_lambda(N,window_lambda,t_star)
-
-  set.seed(12345)
-  RES<- NULL
-  RES<- NSimulations.selon.n(N,window_lambda[1],t_star,k=k,p=p)
-  RES0.2.3<-data.frame(RES)
-  colnames( RES0.2.3)<- c("n","mean.bias")
-  set.seed(12345)
-  RES<- NULL
-  RES<- NSimulations.selon.n(N,window_lambda[2],t_star,k=k,p=p)
-  RES0.5.3<-data.frame(RES)
-  colnames( RES0.5.3)<- c("n","mean.bias")
-
-  set.seed(12345)
-  RES<- NULL
-  RES<- NSimulations.selon.n(N,window_lambda[3],t_star,k=k,p=p)
-  RES0.1.3<-data.frame(RES)
-  colnames( RES0.1.3)<- c("n","mean.bias")
-  borne_min<-min(min(RES0.5.3$mean.bias),min(RES0.1.3$mean.bias),min(RES0.2.3$mean.bias))
-  borne_max<-max(max(RES0.5.3$mean.bias),max(RES0.1.3$mean.bias),max(RES0.5.3$mean.bias))
-  plot(RES0.2.3$n,RES0.2.3$mean.bias,title=paste("Influence of n"),
-       ylim=c(-0.1+borne_min,borne_max+0.1),
-       type='b',bty="n",xlab="nbre sujets",ylab="biais moyen")
-  title("Influence de n et lambda")
-  lines(RES0.5.3$n,RES0.5.3$mean.bias,type="b",col="blue")
-  lines(RES0.1.3$n,RES0.1.3$mean.bias,type="b",col="red")
-  abline(h=0)
-  legend("topright",c("0.1","0.2","0.5"),col=c("red","black","blue"),lty=1,bty="n")
-}
-
 
 
 
